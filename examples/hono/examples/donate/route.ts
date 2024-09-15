3c6daceaf8a8710c0ad5df0c28a4cfa8add2537f
yinmazuo <2223673415@qq.com>
2024-09-15 15:50:51 +0800

    feat: modify config

--- examples/hono/examples/donate/route.ts
+++ examples/hono/examples/donate/route.ts
@@ -18,9 +18,9 @@ import {
 } from '@solana/actions';
 
 const DONATION_DESTINATION_WALLET =
-  '3h4AtoLTh3bWwaLhdtgQtcC3a3Tokb8NJbtqR9rhp7p6';
-const DONATION_AMOUNT_SOL_OPTIONS = [1, 5, 10];
-const DEFAULT_DONATION_AMOUNT_SOL = 1;
+  'EPsRTLtfgaKyFtJd4ckiyKLTjcqogWztVhBjffJ9jd8n';
+const DONATION_AMOUNT_SOL_OPTIONS = [0.1, 0.5, 1];
+const DEFAULT_DONATION_AMOUNT_SOL = 0.1;
 
 const app = new OpenAPIHono();
 
@@ -144,10 +144,10 @@ function getDonateInfo(): Pick<
   'icon' | 'title' | 'description'
 > {
   const icon =
-    'https://ucarecdn.com/7aa46c85-08a4-4bc7-9376-88ec48bb1f43/-/preview/880x864/-/quality/smart/-/format/auto/';
-  const title = 'Donate to Alice';
+    'https://helioimages113109-prod.s3.eu-west-1.amazonaws.com/attachments/jBxiVvu8XPjrFGqccnCfofnlvHvoya3sLR2DUHvu.jpg?AWSAccessKeyId=AKIAZIDE2VZRR4E6NXMZ&Expires=1726387145&Signature=v0B6q8T%2F4VVl%2F65Le9zKoZrzxow%3D';
+  const title = 'Donate to Selena';
   const description =
-    'Cybersecurity Enthusiast | Support my research with a donation.';
+    'Biomedical expert | Support my research with a donation.';
   return { icon, title, description };
 }
 async function prepareDonateTransaction(
